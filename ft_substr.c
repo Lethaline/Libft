6ce6221a1ea2cedfb257eaaa9e156cb8c2c3c168
Louis Lemmens <lolemmen@shi-r1-p1.s19.be>
2022-04-11 15:09:12 +0200

    lolemmen

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: lolemmen <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/07 17:26:48 by lolemmen          #+#    #+#             */
-/*   Updated: 2022/04/08 15:04:45 by lolemmen         ###   ########.fr       */
+/*   Updated: 2022/04/11 10:16:46 by lolemmen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	size_t	i;
 	char	*str;
 
-	i = 0;
 	if (!s)
 		return (NULL);
-	str = (char *)malloc(sizeof(char) * len + 1);
+	if (ft_strlen(s) > len)
+		i = len;
+	else
+		i = ft_strlen(s);
+	str = (char *)malloc(sizeof(char) * i + 1);
 	if (!str)
 		return (NULL);
+	i = 0;
 	if (start > (unsigned int)ft_strlen(s))
 	{
 		str[0] = '\0';
